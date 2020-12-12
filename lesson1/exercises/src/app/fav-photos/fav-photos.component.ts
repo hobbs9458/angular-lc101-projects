d61bafbff0a3da6608f6756c122ccb51920a18a1
Logan Hobbs <hobbs9458@gmail.com>
2020-12-12 12:54:57 -0600

    finished lesson1 exercises

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = 'https://pixabay.com/get/57e6d7414254af14f6d1867dda3536781538dbe25152714e_1920.jpg';
-  image3 = 'https://pixabay.com/get/52e7d5474d55ad14f6d1867dda3536781538dbe25152774d_1920.jpg';
+  image2 = 'https://pixabay.com/get/54e7d5434353a814f6d1867dda3536781538dbed5155744b_1920.jpg';
+  image3 = 'https://pixabay.com/get/52e7d5474d55ad14f6d1867dda3536781538dbed5152764a_1920.jpg';
 
   constructor() { }
 
